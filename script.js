70447fe9a87de68ad0783abfdc8fcc21761ed425
pashakotoff <101649499+pashakotoff@users.noreply.github.com>
2022-08-13 13:47:47 +0300

    Added html, css + base of js

---
+++ script.js
@@ -0,0 +1,53 @@
+function getTemplate (state) {
+    return `
+        <div class="slider_before">
+            <div class="slider_resize" data-type="resize">
+        
+            </div>
+        </div>
+        <div class="slider_after">`
+
+}
+
+
+class Slider {
+    constructor(selector, state) {
+        this.$slider = document.getElementById(selector)
+        this.state = state
+        this.#render(this.state)
+        this.#listen();
+    }
+
+    #render(state) {
+        this.$slider.innerHTML = getTemplate(state)
+    }
+
+    #listen(){
+        this.mouseDownHandler = this.mouseDownHandler.bind(this);
+        this.mouseUpHandler = this.mouseUpHandler.bind(this);
+        this.moveHandler = this.moveHandler.bind(this);
+        this.$slider.addEventListener('mousedown', this.mouseDownHandler);
+        this.$slider.addEventListener('mouseup', this.mouseUpHandler);
+    }
+
+    mouseDownHandler(event){
+        if(event.target.dataset.type === 'resize') {
+            this.$slider.addEventListener('mousemove', this.moveHandler)
+            console.log('down')
+        }
+
+    }
+
+    mouseUpHandler(event) {
+        this.$slider.removeEventListener('mousemove', this.moveHandler)
+        console.log('up')
+
+    }
+
+    moveHandler(event) {
+
+    }
+}
+
+
+const slider = new Slider('slider', {})
